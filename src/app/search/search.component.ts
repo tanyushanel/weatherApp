b8dbf3fa09fa3279376cfc2821f139f95242b22c
tanyushanel <tanyushanel@users.noreply.github.com>
2021-02-13 19:31:27 +0300

    fix

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { WeatherData, WeatherService } from './../weather.service';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
-
-  constructor() { }
+  weatherData: WeatherData;
+  constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
+    this.weatherData.city = this.weatherService.weatherData.city;
   }
-
 }
